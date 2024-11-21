834762edbeaad36f36704b4459df949e02217a7d
Kacper Michalak <michalakkacper2002@gmail.com>
2024-11-21 18:41:40 +0100

    Added list with ability to add new tasks

--- app/screens/TaskDetailsScreen.js
+++ app/screens/TaskDetailsScreen.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 function TaskDetailsScreen({ route, navigation }) {
-    const { taskId, email } = route.params || {};
+    const { taskId, email, status } = route.params || {};
 
     return (
         <View>
             <Text>Email {email}</Text>
             <Text>Szczegóły zadania</Text>
             <Text>ID zadania: {taskId}</Text>
+            <Text>Progress zadania: {status}</Text>
             <Button title='Powrót do listy zadań' onPress={() => navigation.goBack()} />
         </View>
     );
--- app/screens/TaskListScreen.js
+++ app/screens/TaskListScreen.js
@@ -1,19 +1,89 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-
+import { View, Text, Button, Alert } from 'react-native';
+import { ListItem, Icon, FAB } from 'react-native-elements';
+import { useState } from 'react';
 function TaskListScreen({ route, navigation }) {
     const { email } = route.params || {};
-    const handleTaskPress = taskId => {
-        navigation.navigate('TaskDetails', { taskId, email });
+    const handleTaskPress = (taskId,status) => {
+        navigation.navigate('TaskDetails', { taskId, email, status});
     };
+
+    const [list, setList] = useState( [
+        {
+            name: 'Zadanie 1',
+            taskId: 1,
+            status: 'completed',
+        },
+
+        {
+            name: 'Zadanie 2',
+            taskId: 2,
+            status: 'inProgress',
+        }
+    ]);
+
+    const handleAddTask =() =>{
+        const newTask = {
+            name :`Zadanie ${list.length + 1}`,
+            taskId: list.length + 1,
+            status: 'inProgress',
+        };
+
+        setList([...list,newTask]);
+        Alert.alert("Dodano nowe zadanie!");
+        
+    }
+
+    const getStatusIcon = status =>{
+        switch(status){
+            case 'completed':
+                return { name: 'check-circle', color: 'green' };
+            case 'inProgress':
+                return { name: 'pending', color: 'orange' };
+            default:
+                 return { name: 'help', color: 'red' };
+        }
+    }
 
     return (
         <View>
-            <Text>Lista zadań</Text>
-            <Text>Witaj, {email}</Text>
-            <Button title='Otwórz zadanie 1' onPress={() => handleTaskPress(1)} />
-            <Button title='Otwórz zadanie 2' onPress={() => handleTaskPress(2)} />
+            {
+                list.map((item,i)=>(
+                    <ListItem
+                        key={i}
+                        onPress={()=>handleTaskPress(item.taskId,item.status)}
+                        bottomDivider
+                        containerStyle={{ flexDirection: 'row', alignItems: 'center' }}
+                        
+                    >
+                        
+                    <ListItem.Content style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
+                        <ListItem.Title>{item.name}</ListItem.Title>
+                        <Icon
+                            name={getStatusIcon(item.status).name}
+                            color={getStatusIcon(item.status).color}
+                            containerStyle={{ marginLeft: 10 }}
+                            />
+                        <ListItem.Chevron color='gray' size={20} style={{ marginLeft:'auto' }}/>
+
+                    </ListItem.Content>
+                    </ListItem>
+
+                ))
+            }
+            <FAB
+                title='+'
+                placement='right'
+                color="#0096FF"
+                onPress={handleAddTask}
+                style = {{position: 'absolute',
+                    bottom: 20,
+                    right: 20,}}
+            />
+
         </View>
+
+    
     );
 }
 
