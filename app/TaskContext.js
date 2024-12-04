60816a8e882106e8b78324dbb638a37533a03557
Kacper Michalak <michalakkacper2002@gmail.com>
2024-12-04 21:48:58 +0100

    Added posibility to change task status

--- app/TaskContext.js
+++ app/TaskContext.js
@@ -8,7 +8,16 @@ export const TaskProvider = ({ children }) => {
         { name: 'Zadanie 2', taskId: 2, status: 'inProgress' }
     ]);
 
-    return <TaskContext.Provider value={{ tasks, setTasks }}>{children}</TaskContext.Provider>;
+    const updateTaskStatus = (taskId, newStatus) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.taskId === taskId ? { ...task, status: newStatus } : task
+            )
+        );
+    };
+
+
+    return <TaskContext.Provider value={{ tasks, setTasks, updateTaskStatus }}>{children}</TaskContext.Provider>;
 };
 
 export const useTasks = () => useContext(TaskContext);
--- app/screens/TaskDetailsScreen.js
+++ app/screens/TaskDetailsScreen.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
+import { Switch } from 'react-native-elements';
+import { useTasks } from '../TaskContext';
 
 function TaskDetailsScreen({ route, navigation }) {
-    const { taskId, email, status } = route.params || {};
+    const { taskId, email,status: initialStatus } = route.params || {};
+    const [status,setStatus] = useState(initialStatus);
+
+    const {updateTaskStatus} = useTasks();
+
+    const toggleSwitch = () => {
+        const newStatus = status === 'completed' ? 'inProgress' : 'completed';
+        setStatus(newStatus);
+        updateTaskStatus(taskId,newStatus);
+        
+    };
 
     return (
         <View>
@@ -10,6 +22,13 @@ function TaskDetailsScreen({ route, navigation }) {
             <Text>Szczegóły zadania</Text>
             <Text>ID zadania: {taskId}</Text>
             <Text>Progress zadania: {status}</Text>
+            <Switch 
+                 trackColor={{false: '#767577', true: '#81b0ff'}}
+                 thumbColor={status === 'completed'? '#2596be' : '#f4f3f4'}
+                 ios_backgroundColor="#3e3e3e"
+                 onValueChange={toggleSwitch}
+                 value={status === 'completed'}
+            />
             <Button title='Powrót do listy zadań' onPress={() => navigation.goBack()} />
         </View>
     );
